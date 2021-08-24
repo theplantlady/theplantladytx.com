2a395bb9d5c49fcbede7ca9f4e9ac5af38dea5ad
theplantlady <jeremy@theplantladytx.com>
2021-08-24 02:24:23 -0500

    1 file modified, 1 added

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <script setup>
 import ButtonRepo from '@/components/ButtonRepo.vue'
 import LeftAlignedWImageAndFloatingCard from '@/components/LeftAlignedWImageAndFloatingCard.vue'
+import SimpleBrandedWithStackedLinks from '@/components/SimpleBrandedWithStackedLinks.vue'
+
 </script>
 
 <template>
   <div>
     <header class="bg-white shadow" v-if="$route.meta.title">
+    <SimpleBrandedWithStackedLinks />
       <!-- <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
         <h1 class="text-3xl font-bold leading-tight text-gray-900">{{ $route.meta.title }}</h1>
       </div> -->
