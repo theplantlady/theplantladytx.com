92188c5798866296094dae1b9e008f40e11f6025
theplantlady <jeremy@theplantladytx.com>
2021-08-24 02:12:44 -0500

    2 files modified

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,18 +4,16 @@ import LeftAlignedWImageAndFloatingCard from '@/components/LeftAlignedWImageAndF
 </script>
 
 <template>
+  <LeftAlignedWImageAndFloatingCard />
+
   <div class="bg-gray-50">
-    <div
-      class="max-w-screen-xl px-4 py-12 mx-auto sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between"
-    >
-      <h2
-        class="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 sm:text-4xl sm:leading-10"
-      >
+    <div class="max-w-screen-xl px-4 py-12 mx-auto sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
+      <h2 class="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 sm:text-4xl sm:leading-10">
         Ready to dive in?
         <br />
         <span class="text-indigo-600">Vite + Vue 3 + Tailwind CSS</span>
       </h2>
-      <LeftAlignedWImageAndFloatingCard />
+      
       <div class="flex mt-8 lg:flex-shrink-0 lg:mt-0">
         <div class="inline-flex rounded-md shadow">
           <router-link
