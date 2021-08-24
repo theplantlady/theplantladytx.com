3e158b91793da7028701f1648a732903e09509f1
theplantlady <jeremy@theplantladytx.com>
2021-08-24 02:18:09 -0500

    2 files modified

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,9 @@
 <script setup>
 import ButtonRepo from '@/components/ButtonRepo.vue'
-import LeftAlignedWImageAndFloatingCard from '@/components/LeftAlignedWImageAndFloatingCard.vue'
 </script>
 
 <template>
-  <LeftAlignedWImageAndFloatingCard />
+
 
   <div class="bg-gray-50">
     <div class="max-w-screen-xl px-4 py-12 mx-auto sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
