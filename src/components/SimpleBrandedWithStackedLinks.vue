639f5b5eac676c3b42a189b1d579ce2f4a3356b9
theplantlady <jeremy@theplantladytx.com>
2021-08-24 04:52:58 -0500

    1 file modified

--- src/components/SimpleBrandedWithStackedLinks.vue
+++ src/components/SimpleBrandedWithStackedLinks.vue
@@ -6,7 +6,7 @@
         <div class="flex items-center">
           <a href="#">
             <span class="sr-only">Workflow</span>
-            <img class="h-16 w-auto -my-4" src="https://firebasestorage.googleapis.com/v0/b/xsqrd-inc.appspot.com/o/Asset%202.png?alt=media&token=675b90ac-18bc-4345-9de1-9673f7302508" alt="" />
+            <img class="h-12 w-auto -my-4" src="https://firebasestorage.googleapis.com/v0/b/xsqrd-inc.appspot.com/o/ThePlantLadyTextwSecond.png?alt=media&token=da41f2f0-1cea-4d93-bec1-1b1c646fe2a9" alt="" />
             
             <!-- <img class="h-10 w-auto" src="https://firebasestorage.googleapis.com/v0/b/xsqrd-inc.appspot.com/o/Logo%402x.png?alt=media&token=d88546cd-c8c4-4d8f-9164-504c3a476012" alt="" /> -->
           </a>
